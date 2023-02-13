8e0c5c34cc53aa3b71f5671deabf8493434d50d5
7VIKAS <yadav07v@gmail.com>
2023-02-13 16:45:29 +0530

    modified code

--- new.java
+++ new.java
@@ -7,3 +7,10 @@ public class Main{
         System.out.println(s);
     }
 }
+static public void main(String  args[]){
+        System.out.println("welcome to\nvikas yadav");
+        String s="hello world";
+        System.out.println(s);
+        s="welcome to back in lucknow";
+        System.out.println(s);
+    }
