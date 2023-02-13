61bc5061792033dd5a2771e721588e09a13e87d0
7VIKAS <yadav07v@gmail.com>
2023-02-13 15:30:18 +0530

    first commit

---
+++ new.java
@@ -0,0 +1,9 @@
+public class Main{
+     static public void main(String  args[]){
+        System.out.println("welcome to\nvikas yadav");
+        String s="hello world";
+        System.out.println(s);
+        s="welcome to back in lucknow";
+        System.out.println(s);
+    }
+}
